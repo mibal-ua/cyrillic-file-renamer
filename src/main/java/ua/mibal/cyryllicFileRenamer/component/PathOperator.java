d1d07eec1696cc39d43b83819425a9a2fdd20699
mibal_ua <9mohapx9@gmail.com>
2022-09-02 17:58:22 +0200

    Move testPath method to new PathOperator class

---
+++ src/main/java/ua/mibal/cyryllicFileRenamer/component/PathOperator.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022. http://t.me/mibal_ua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package ua.mibal.cyryllicFileRenamer.component;
+
+import java.io.File;
+
+/**
+ * @author Michael Balakhon
+ * @link http://t.me/mibal_ua
+ */
+public class PathOperator {
+    public String testPath(final String userPath) {
+        if (userPath.length() != 0) {
+            StringBuilder userPathBuilder1 = new StringBuilder(userPath);
+            StringBuilder userPathBuilder2 = new StringBuilder(userPath);
+
+            if (userPathBuilder1.charAt(0) != '/' && userPathBuilder1.charAt(0) != '\\') {
+                userPathBuilder1.insert(0, '/');
+                userPathBuilder1.insert(0, '\\');
+            }
+            if (new File(userPathBuilder1.toString()).exists()) {
+                return userPathBuilder1.toString();
+            } else if (new File(userPathBuilder2.toString()).exists()) {
+                return userPathBuilder2.toString();
+            }
+        }
+        return null;
+    }
+}
