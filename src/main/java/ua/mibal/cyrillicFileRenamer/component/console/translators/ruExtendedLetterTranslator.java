28063c9e4a39552048d524f8d184b51d17e6649b
mibal_ua <9mohapx9@gmail.com>
2022-09-14 11:04:53 +0200

    Implement LetterTranslators with override method

---
+++ src/main/java/ua/mibal/cyrillicFileRenamer/component/console/translators/ruExtendedLetterTranslator.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022. http://t.me/mibal_ua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package ua.mibal.cyrillicFileRenamer.component.console.translators;
+
+import ua.mibal.cyrillicFileRenamer.model.exceptions.IllegalLanguageException;
+import ua.mibal.cyrillicFileRenamer.model.exceptions.IllegalNameException;
+
+import static ua.mibal.cyrillicFileRenamer.model.programMode.Lang.RU;
+
+/**
+ * @author Michael Balakhon
+ * @link http://t.me/mibal_ua
+ */
+public class ruExtendedLetterTranslator extends LetterTranslator {
+
+    public ruExtendedLetterTranslator() {
+        super();
+    }
+
+    @Override
+    protected String translate(final String word, final int i, final String letter) throws IllegalNameException, IllegalLanguageException {
+        if (isSpecialLetter(letter)) { // if lang ru and official - return false
+            if (i == 0) {
+                return translateSpecialSymbols(letter, RU);
+            } else if (isGolosnyy(word.charAt(i - 1)) || isZnakMyakshenniaOrElse(word.charAt(i - 1))) {
+                return translateSpecialSymbols(letter, RU);
+            } else {
+                return convertFromru(letter);
+            }
+        } else if (i != 0 && letter.equalsIgnoreCase("И") &&
+                   isShyplyachyy(word.charAt(i - 1))) {
+            return Character.isUpperCase(letter.charAt(0)) ? "Y" : "y";
+        } else {
+            return convertFromru(letter);
+        }
+    }
+}
---
+++ src/main/java/ua/mibal/cyrillicFileRenamer/component/console/translators/ruOfficialLetterTranslator.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022. http://t.me/mibal_ua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package ua.mibal.cyrillicFileRenamer.component.console.translators;
+
+import ua.mibal.cyrillicFileRenamer.model.exceptions.IllegalLanguageException;
+import ua.mibal.cyrillicFileRenamer.model.exceptions.IllegalNameException;
+
+import static ua.mibal.cyrillicFileRenamer.model.programMode.Lang.RU;
+
+/**
+ * @author Michael Balakhon
+ * @link http://t.me/mibal_ua
+ */
+public class ruOfficialLetterTranslator extends LetterTranslator {
+
+    @Override
+    protected String translate(final String word, final int i, final String letter) throws IllegalLanguageException, IllegalNameException {
+        return convertFromOfficialru(letter);
+    }
+
+    private String convertFromOfficialru(final String ch) throws IllegalLanguageException {
+        String result = switch (ch.toUpperCase()) {
+            case "Г" -> "G";
+            case "Ё", "Э" -> "E";
+            case "И", "Й" -> "I";
+            case "Ы" -> "Y";
+            case "Ъ" -> "Ie";
+            default -> convertUniversal(ch, RU);
+        };
+        return Character.isUpperCase(ch.charAt(0)) ? result : result.toLowerCase();
+    }
+}
